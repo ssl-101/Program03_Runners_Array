db04a1088b62afb74c030ca6d2ff8b33d3a4848a
ssl-101 <inquiresiham@gmail.com>
2026-09-17 16:02:39 -0400

    First for loop established for the rows and columns.

--- Runners.cpp
+++ Runners.cpp
@@ -3,4 +3,20 @@
 #include <fstream>
 #include <iomanip>
 
+using namespace std;
 
+bool readRunnerData (const string& filename, string names[], double miles[][DAYS_OF_WEEK]){
+    ifstream inFile(runners.txt);
+    if (!inFile){
+        return false;
+    }
+
+    for(int row = 0; row < NUM_RUNNERS; ++row){
+        inFile >> names[row];
+        for (int col = 0; col < DAYS_OF_WEEK; ++col){
+            sum += miles[row][col];
+        }
+    }
+    inFile.close();
+    return true;
+}
