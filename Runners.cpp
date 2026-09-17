7506e6d78c698696a7e79dec33fd31fe2b9df5a6
ssl-101 <inquiresiham@gmail.com>
2026-09-17 17:56:25 -0400

    completed for loops. Calculations of total and average.

--- Runners.cpp
+++ Runners.cpp
@@ -14,11 +14,21 @@ bool readFile(const string& filename, string names[], double miles[][DAYS_OF_WEE
     for(int row = 0; row < NUM_RUNNERS; ++row){
         inFile >> names[row];
         for (int col = 0; col < DAYS_OF_WEEK; ++col){
-            sum += miles[row][col];
+            inFile >> miles[row][col];
         }
     }
     inFile.close();
     return true;
 }
 //total calculation and average calculations
-void calculat
+void calculateTotals(const double miles[][DAYS_OF_WEEK], double totals[], double averages[]){
+     for (int row = 0; row < NUM_RUNNERS; ++row){
+        double sum = 0.0;
+        for (int col = 0; col < DAYS_OF_WEEK; ++col){
+            sum +== miles[row][col];
+        }
+        totals[row] = sum;
+        averages[row] = sum / DAYS_OF_WEEK;
+
+     }
+}
